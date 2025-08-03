988f925bb2882e73a37b0dd3304a48f47e52aa72
Dypose <d.bolonkin0022@gmail.com>
2025-08-03 15:12:24 +0300

    add IoC

--- src/main/java/ru/dypose/springcore/TestSpring.java
+++ src/main/java/ru/dypose/springcore/TestSpring.java
@@ -1,13 +1,20 @@
 package ru.dypose.springcore;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.dypose.springcore.inversionOfControl.Music;
+import ru.dypose.springcore.inversionOfControl.MusicPlayer;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        context.close();
+
+        try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml")) {
+            Music music =context.getBean("musicBean", Music.class);
+            MusicPlayer musicPlayer = new MusicPlayer(music);
+            musicPlayer.play();
+
+        }
+
+
     }
 }
