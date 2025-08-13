16d54e76db7d69fbc2408f03a5dbcd5df76c1e1c
Dypose <d.bolonkin0022@gmail.com>
2025-08-14 00:05:08 +0300

    @Qualifier помогает решить проблемы 1)Когда реализуем абстрактный интерфейс/класс где аннотация @Component>1 Spring не может понять какой именно нужен бин 2)Явное указание реализации бина

--- src/main/java/ru/dypose/springcore/task/MusicPlayer.java
+++ src/main/java/ru/dypose/springcore/task/MusicPlayer.java
@@ -1,9 +1,9 @@
 package ru.dypose.springcore.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.dypose.springcore.beans.Music;
+
 @Component
 public class MusicPlayer {
 
@@ -12,20 +12,25 @@ public class MusicPlayer {
     private Music music3;
 
     public MusicPlayer(@Qualifier("classicalMusic") Music music1
-            ,@Qualifier("hipHopMusic") Music music2
-            ,@Qualifier("rockMusic") Music music3) {
+            , @Qualifier("hipHopMusic") Music music2
+            , @Qualifier("rockMusic") Music music3) {
         this.music1 = music1;
         this.music2 = music2;
         this.music3 = music3;
     }
-    public String getSoundMusic(TypeMisic typeMisic){
+
+    public String getSoundMusic(TypeMisic typeMisic) {
         int ordinal = typeMisic.ordinal();
 
-        switch (ordinal){
-            case 0:return music1.getSong();
-            case 1:return music2.getSong();
-            case 2:return music3.getSong();
-            default: return "";
+        switch (ordinal) {
+            case 0:
+                return music1.getSong();
+            case 1:
+                return music2.getSong();
+            case 2:
+                return music3.getSong();
+            default:
+                return "";
         }
 
     }
